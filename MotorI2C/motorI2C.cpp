7bdb3ee084792bcba063aaa19e34f64b4e6b8c55
Davide Covolo <164493065+Disteroas@users.noreply.github.com>
2024-11-06 22:38:12 +0100

    Update motorI2C.cpp
    We've corrected some of the issues commented by @luca-byte. In the next days we'll correct all of them

--- MotorI2C/motorI2C.cpp
+++ MotorI2C/motorI2C.cpp
@@ -1,19 +1,23 @@
+#include <Wire.h>
 #include "MotorI2C.h"
 
 // Create a custom I2C instance for the Pico (as slave)
 TwoWire I2C_Slave = TwoWire(0);  // Use bus 0 for the I2C
 
-MotorI2C::MotorI2C(TwoWire& wire, int slaveAddress) 
-  : wireInstance(wire), slaveAddress(slaveAddress), receivedLeftSpeed(0), receivedRightSpeed(0), newDataAvailable(false), lastCommandTime(0) {}
+// Constructor now takes motor pointers as arguments
+MotorI2C::MotorI2C(TwoWire& wire, int slaveAddress, SmartMotor* leftMotor, SmartMotor* rightMotor)
+  : wireInstance(wire), slaveAddress(slaveAddress), leftMotor(leftMotor), rightMotor(rightMotor),
+    receivedLeftSpeed(0), receivedRightSpeed(0), newDataAvailable(false), lastCommandTime(0) {}
 
 // Setup I2C communication
 void MotorI2C::begin() {
-  wireInstance.begin(slaveAddress);  // Initialize the I2C as a slave
-  wireInstance.onReceive(receiveData);  // Set the receive function to handle incoming data
-  wireInstance.onRequest(sendTelemetry); // Set the request function to handle telemetry data
+  wireInstance.onReceive([this](int numBytes) { receiveData(numBytes); });  // Set receive handler with lambda
+  wireInstance.onRequest([this]() { sendTelemetry(); }); // Set request handler with lambda
 }
 
 // Function to receive data from the master
+
+/*
 void MotorI2C::receiveData(int numBytes) {
   if (numBytes >= 2 * sizeof(float)) {  // Expect two floats (leftSpeed, rightSpeed)
     wireInstance.readBytes((char*)&receivedLeftSpeed, sizeof(float));  // Read left motor speed
@@ -23,11 +27,12 @@ void MotorI2C::receiveData(int numBytes) {
     lastCommandTime = millis();  // Update the time of the last received command
   }
 }
+*/
 
 // Function to send telemetry (current speeds) back to the master
 void MotorI2C::sendTelemetry() {
-  float leftSpeed = receivedLeftSpeed;
-  float rightSpeed = receivedRightSpeed;
+   float leftSpeed = leftMotor->getSpeed();
+  float rightSpeed = rightMotor->getSpeed();
 
   // Send back two floats (current speeds)
   wireInstance.write((byte*)&leftSpeed, sizeof(float));
@@ -46,7 +51,7 @@ bool MotorI2C::isAvailable(float* leftSpeed, float* rightSpeed) {
 }
 
 // Function to check if motors should be stopped due to timeout
-void MotorI2C::checkSafety(SmartMotor* leftMotor, SmartMotor* rightMotor) {
+void MotorI2C::checkSafety() {
   unsigned long currentTime = millis();
   if (currentTime - lastCommandTime > MOTOR_TIMEOUT) {
     leftMotor->stop();
