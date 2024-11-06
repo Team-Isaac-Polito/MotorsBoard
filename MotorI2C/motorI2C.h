c84f3c464594fb80883c049130a9b144009fc838
Davide Covolo <164493065+Disteroas@users.noreply.github.com>
2024-11-06 22:38:44 +0100

    Update motorI2C.h

--- MotorI2C/motorI2C.h
+++ MotorI2C/motorI2C.h
@@ -8,20 +8,22 @@
 
 class MotorI2C {
   public:
-    MotorI2C(TwoWire& wire, int slaveAddress);
-    void begin(); // Initialize I2C
-    void receiveData(int numBytes);        // Receive speed data from the main board
-    void sendTelemetry();                 // Send telemetry (speed) data back to the main board
+    MotorI2C(TwoWire& wire, int slaveAddress, SmartMotor* leftMotor, SmartMotor* rightMotor);
+    void begin();                        // Initialize I2C
+    void receiveData(int numBytes);      // Receive speed data from the main board
+    void sendTelemetry();                // Send telemetry (speed) data back to the main board
     bool isAvailable(float* leftSpeed, float* rightSpeed);  // Check if new data is available
-    void checkSafety(SmartMotor* leftMotor, SmartMotor* rightMotor);  // Check for motor timeout
+    void checkSafety();                  // Check for motor timeout
 
   private:
     TwoWire& wireInstance;
     int slaveAddress;
-    float receivedLeftSpeed;              // Received left motor speed
-    float receivedRightSpeed;             // Received right motor speed
-    bool newDataAvailable;                // Flag to indicate new data has been received
-    unsigned long lastCommandTime;        // Time of the last received command
+    SmartMotor* leftMotor;               // Pointer to the left motor
+    SmartMotor* rightMotor;              // Pointer to the right motor
+    float receivedLeftSpeed;             // Received left motor speed
+    float receivedRightSpeed;            // Received right motor speed
+    bool newDataAvailable;               // Flag to indicate new data has been received
+    unsigned long lastCommandTime;       // Time of the last received command
 };
 
 #endif
