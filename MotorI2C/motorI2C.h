3b939ebeaec59c7fd49f2bae9c95cf331d6868f7
Davide Covolo <164493065+Disteroas@users.noreply.github.com>
2024-11-11 17:04:48 +0100

    Update motorI2C.h

--- MotorI2C/motorI2C.h
+++ MotorI2C/motorI2C.h
@@ -9,21 +9,20 @@
 class MotorI2C {
   public:
     MotorI2C(TwoWire& wire, int slaveAddress, SmartMotor* leftMotor, SmartMotor* rightMotor);
-    void begin();                        // Initialize I2C
-    void receiveData(int numBytes);      // Receive speed data from the main board
-    void sendTelemetry();                // Send telemetry (speed) data back to the main board
-    bool isAvailable(float* leftSpeed, float* rightSpeed);  // Check if new data is available
-    void checkSafety();                  // Check for motor timeout
+    void begin();                         // Initialize I2C
+    void updateMotors();                  // Update motor speeds and check for timeout
 
   private:
-    TwoWire& wireInstance;
+    void receiveData(int numBytes);       // Receive speed data from the main board
+    void sendTelemetry();                 // Send telemetry (speed) data back to the main board
+    TwoWire& wireInstance;                // I2C instance
     int slaveAddress;
-    SmartMotor* leftMotor;               // Pointer to the left motor
-    SmartMotor* rightMotor;              // Pointer to the right motor
-    float receivedLeftSpeed;             // Received left motor speed
-    float receivedRightSpeed;            // Received right motor speed
-    bool newDataAvailable;               // Flag to indicate new data has been received
-    unsigned long lastCommandTime;       // Time of the last received command
+    SmartMotor* leftMotor;                // Pointer to the left motor
+    SmartMotor* rightMotor;               // Pointer to the right motor
+    float receivedLeftSpeed;              // Received left motor speed
+    float receivedRightSpeed;             // Received right motor speed
+    bool newDataAvailable;                // Flag to indicate new data has been received
+    unsigned long lastCommandTime;        // Time of the last received command
 };
 
 #endif
